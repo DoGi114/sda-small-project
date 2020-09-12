7c3f2770c84f733c33694c2bd7e5110567c5a7a0
Damian Nguyen <damian.nguyen93@gmail.com>
2020-09-12 10:00:28 +0200

    Initial commit

---
+++ src/test/java/EmployeeTest.java
@@ -0,0 +1,193 @@
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Employee Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>wrz 12, 2020</pre> 
+* @version 1.0 
+*/ 
+public class EmployeeTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getId() 
+* 
+*/ 
+@Test
+public void testGetId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setId(Long id) 
+* 
+*/ 
+@Test
+public void testSetId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getFirstName() 
+* 
+*/ 
+@Test
+public void testGetFirstName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setFirstName(String firstName) 
+* 
+*/ 
+@Test
+public void testSetFirstName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getLastName() 
+* 
+*/ 
+@Test
+public void testGetLastName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setLastName(String lastName) 
+* 
+*/ 
+@Test
+public void testSetLastName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPosition() 
+* 
+*/ 
+@Test
+public void testGetPosition() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPosition(String position) 
+* 
+*/ 
+@Test
+public void testSetPosition() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getSalary() 
+* 
+*/ 
+@Test
+public void testGetSalary() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSalary(Double salary) 
+* 
+*/ 
+@Test
+public void testSetSalary() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getBirthYear() 
+* 
+*/ 
+@Test
+public void testGetBirthYear() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setBirthYear(LocalDate birthYear) 
+* 
+*/ 
+@Test
+public void testSetBirthYear() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getVacations() 
+* 
+*/ 
+@Test
+public void testGetVacations() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setVacations(List<Vacation> vacations) 
+* 
+*/ 
+@Test
+public void testSetVacations() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: equals(Object o) 
+* 
+*/ 
+@Test
+public void testEquals() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: hashCode() 
+* 
+*/ 
+@Test
+public void testHashCode() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: toString() 
+* 
+*/ 
+@Test
+public void testToString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
---
+++ src/test/java/VacationTest.java
@@ -0,0 +1,113 @@
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Vacation Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>wrz 12, 2020</pre> 
+* @version 1.0 
+*/ 
+public class VacationTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getOwner() 
+* 
+*/ 
+@Test
+public void testGetOwner() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setOwner(Employee owner) 
+* 
+*/ 
+@Test
+public void testSetOwner() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getStarts() 
+* 
+*/ 
+@Test
+public void testGetStarts() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setStarts(LocalDate starts) 
+* 
+*/ 
+@Test
+public void testSetStarts() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getEnds() 
+* 
+*/ 
+@Test
+public void testGetEnds() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setEnds(LocalDate ends) 
+* 
+*/ 
+@Test
+public void testSetEnds() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: equals(Object o) 
+* 
+*/ 
+@Test
+public void testEquals() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: hashCode() 
+* 
+*/ 
+@Test
+public void testHashCode() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: toString() 
+* 
+*/ 
+@Test
+public void testToString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
